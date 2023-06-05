c01e724de094528513b0142a29733bfba5972a6e
Abu A'la <abualaanadwi1@gmail.com>
2023-06-06 05:23:22 +0700

    Early commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const boardStyle = {
+  backgroundColor: 'grey',
+  margin: 10,
+  padding: 20
+}
+const Square = (props) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <button 
+    className='square'
+    onClick={props.onClickEvent}>
+      {props.value}
+    </button>
+  )
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const Board = () => {
+
+  const initialSquare = Array(9).fill(null);
+
+  const [squares, setSquares] = useState(initialSquare);
+  const [nextPlayer, setNextPlayer] = useState(true);
+
+  const handleClick = (i) => {
+    const newSquare = [...squares];
+    const winnerDeclared = Boolean(calculateWinner(newSquare));
+    const squareFilled = Boolean(newSquare[i]);
+
+    if (winnerDeclared || squareFilled) {
+      return;
+    }
+
+
+    newSquare[i] = nextPlayer ? 'X' : 'O';
+    setSquares(newSquare);
+    setNextPlayer(!nextPlayer);
+  }
+
+  const renderSquare = (i) => {
+    return (
+          <Square 
+          value={squares[i]}
+          onClickEvent = {() => handleClick(i)} />
+        );
+  }
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player is: ${nextPlayer ? 'X' : 'O'}`;
+  
+  return (
+      <div style={boardStyle}>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+        </div>
+      </div>
+  );
+}
+
+
+const Game = () => {
+  return (
+      <div className='game'>
+        Tic-Tac-Toe
+        <Board />
+      </div>
+  );
+};
+
+ReactDOM.render(<Game />, document.getElementById('root'));
